5b760484ec152b6fdf668c056d94e59343127302
Ozgun Balaban <ozgunbalaban@yahoo.com>
2019-10-16 22:12:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,9 +1,12 @@
 function forLoop(array) {
-  for (let i = 1; i < 25; i++) {
-   array[i] = `I am ${i} strange loop${i === 1 ? '' : 's'}.`
-   console.log(array)
-}
-return array
+for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
 
 function whileLoop (n) {
